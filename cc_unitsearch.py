c151ee1d87c646ec3382be11b92baf1701b6ecf5
DeathWish99 <44440688+DeathWish99@users.noreply.github.com>
2022-04-25 17:42:59 +0800

    Refactored code into functions

--- cc_unitsearch.py
+++ cc_unitsearch.py
@@ -22,14 +22,31 @@ class CustomFilter(QSortFilterProxyModel):
         return ((self.filterRegExp().indexIn(self.sourceModel().data(index0, self.role)) >= 0 or self.filterRegExp().indexIn(self.sourceModel().data(index1, self.role)) >= 0))
 
 class CCSearch(QWidget):
-    filter_proxy_model = CustomFilter()
     def __init__(self):
         super().__init__()
 
         self.resize(1280, 720)
+        self.filter_proxy_model = CustomFilter()
+        
         mainLayout = QVBoxLayout()
         units = pullFromJson()
 
+        model = self.createUnitItems(units=units)
+
+        self.createFilteredModel(model=model)
+
+        table = self.createTable()
+        
+        search_field = QLineEdit()
+        search_field.setStyleSheet('font-size: 35px; height: 60px')
+        search_field.textChanged.connect(self.onTextChanged)
+        mainLayout.addWidget(search_field)
+
+        mainLayout.addWidget(table)
+
+        self.setLayout(mainLayout)
+
+    def createUnitItems(self, units):
         model = QStandardItemModel(len(units), 3)
         model.setHorizontalHeaderLabels(['CC Unit', 'Attila Unit', 'Tier & Type'])
         for row, unit in enumerate(units):
@@ -39,25 +56,21 @@ class CCSearch(QWidget):
             model.setItem(row, 0, ccUnit)
             model.setItem(row, 1, attilaUnit)
             model.setItem(row, 2, unitType)
+        return model
 
+    def createFilteredModel(self, model):
         self.filter_proxy_model.setSourceModel(model)
         self.filter_proxy_model.setFilterCaseSensitivity(Qt.CaseInsensitive)
         self.filter_proxy_model.setFilterKeyColumn(0)
 
-        search_field = QLineEdit()
-        search_field.setStyleSheet('font-size: 35px; height: 60px')
-        search_field.textChanged.connect(self.onTextChanged)
-        mainLayout.addWidget(search_field)
-
+    def createTable(self):
         table = QTableView()
         table.setStyleSheet('font-size: 35px')
         table.verticalHeader().setSectionResizeMode(QHeaderView.Stretch)
         table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
         table.setModel(self.filter_proxy_model)
-        mainLayout.addWidget(table)
-
-        self.setLayout(mainLayout)
-
+        return table
+        
     def onTextChanged(self, text):
         self.filter_proxy_model.setFilterRegExp(QRegExp(text, Qt.CaseInsensitive, QRegExp.FixedString))
 
